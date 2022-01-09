2633de2e6b57199c82129c832511ebd2ad44f59f
Tucker Craig <tuckermcraig@gmail.com>
2022-01-09 18:22:18 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
 import Grid from './Grid/Grid';
-import {useState} from 'react';
+import Error from './Error/Error';
+import {useState, useEffect} from 'react';
+import $ from 'jquery';
 
 function App() {
 
-	const [order, setOrder] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
-
+	const [order, setOrder] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 	const [inputValue, setInputValue] = useState();
+	const [hasFocus, setFocus] = useState(false);
+	const [error, setError] = useState(null);
+	const [isActive, setIsActive] = useState(false);
 
 	const rearrange = () => {
+
+		setIsActive(false);
+
+		if (!inputValue) {
+			setError('You must enter a sequence of numbers');
+			setIsActive(true);
+			return;
+		}
+
 		const newOrder = inputValue.split('').map(x => +x);
 		
-		if (
-			newOrder.length !== 9 || 
-			newOrder.includes(NaN) || 
+		if (newOrder.length !== 9) {
+			setError('There must be exactly 9 numbers');
+			setIsActive(true);
+			return;
+		} else if (newOrder.includes(NaN)) {
+			setError('All characters must be a number');
+			setIsActive(true);
+			return;
+		}else if (
 			!newOrder.includes(1) || 
 			!newOrder.includes(2) || 
 			!newOrder.includes(3) || 
@@ -24,25 +43,36 @@ function App() {
 			!newOrder.includes(8) || 
 			!newOrder.includes(9)
 		) {
-			console.log('ERROR');
+			setError('There must be no duplicates');
+			setIsActive(true);
 			return;
 		}
 		setOrder(newOrder);
 	};
 
-	const inFocus = $('#input').is(':focus')
-
 	const handleKeypress = e => {
-		if (e.keyCode === 13 && inFocus === true) {
+		if (e.key === 'Enter') {
 			rearrange()
-		}
-	}
+		};
+	};
 
   	return (
     	<div className="App">
+			<Error isActive={isActive} error={error} />
 			<Grid order = {order} />
 			<btn className='child' onClick={() => rearrange()}>Re-Arrange</btn>
-			<input onKeyPress={handleKeypress} className='child' id='input' onChange={e => setInputValue(e.target.value)} type="text" placeholder='ex. 123456789' minLength="9" maxLength="9"></input>
+			<input 
+				onKeyPress={handleKeypress} 
+				onFocus={() => setFocus(true)} 
+				onBlur={() => setFocus(false)} 
+				className='child input' 
+				id='input' 
+				onChange={e => setInputValue(e.target.value)} 
+				type="text" 
+				placeholder='ex. 123456789' 
+				minLength="9" 
+				maxLength="9"
+			></input>
     	</div>
   	);
 };
